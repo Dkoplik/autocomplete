1f2196cef380144cf7c63bf86461176848d05985
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-06-12 14:52:14 +0300

    add trie (#5)

---
+++ src/main/java/io/github/autocomplete/TrieNode.java
@@ -0,0 +1,33 @@
+package io.github.autocomplete;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Узел префиксного дерева
+ */
+class TrieNode {
+    private final Map<Character, TrieNode> children;
+    private int frequency;
+
+    public TrieNode() {
+        this.children = new HashMap<>();
+        this.frequency = 0;
+    }
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public void incrementFrequency() {
+        this.frequency++;
+    }
+}
