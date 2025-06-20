bee242fd5693e989000615acab76304f5138f9e0
Ruslan2806 <115139612+Ruslan2806@users.noreply.github.com>
2025-06-20 13:50:34 +0300

    corrected comments and "processWord" method

--- src/main/java/io/github/autocomplete/tokenizer/TokenizerConfig.java
+++ src/main/java/io/github/autocomplete/tokenizer/TokenizerConfig.java
@@ -3,13 +3,16 @@ package io.github.autocomplete.tokenizer;
 import java.util.function.Predicate;
 
 /**
- * Конфигурация токенизатора с настраиваемыми параметрами
+ * Конфигурация токенизатора с настраиваемыми параметрами.
+ *
+ * @param splitRegex Regex выражение, по которому происходит разделение текста на слова (токены). По
+ *        умолчанию - пробельные символы.
+ * @param charFilter Предикат для фильтрации, выбирает, какие символы оставлять в итоговых токенах.
+ *        По умолчанию - только буквенные символы.
+ * @param toLowerCase Приводить ли все слова к нижнему регистру. По умолчанию - true.
  */
-public class TokenizerConfig {
-    private final String splitRegex;
-    private final Predicate<Character> charFilter;
-    private final boolean toLowerCase;
-
+public record TokenizerConfig(String splitRegex, Predicate<Character> charFilter,
+        boolean toLowerCase) {
     /**
      * Конфигурация токенизатора со стандартными настройками.
      * <p>
@@ -18,40 +21,5 @@ public class TokenizerConfig {
      */
     public TokenizerConfig() {
         this("\\s+", Character::isLetter, true);
-    }
-
-    /**
-     * Конфигурация токенизатора со своими настройками
-     * 
-     * @param splitRegex Regex выражение, по которому происходит разделение на слова
-     * @param charFilter Предикат для фильтрации, выбирает, какие символы оставлять
-     * @param toLowerCase Приводить ли все слова к нижнему регистру или оставить как есть
-     */
-    public TokenizerConfig(String splitRegex, Predicate<Character> charFilter,
-            boolean toLowerCase) {
-        this.splitRegex = splitRegex;
-        this.charFilter = charFilter;
-        this.toLowerCase = toLowerCase;
-    }
-
-    /**
-     * @return Regex выражение, по которому происходит разделение на слова
-     */
-    public String getSplitRegex() {
-        return splitRegex;
-    }
-
-    /**
-     * @return Предикат для фильтрации, выбирает, какие символы оставлять
-     */
-    public Predicate<Character> getCharFilter() {
-        return charFilter;
-    }
-
-    /**
-     * @return True, если приводит все слова к нижнему регистру
-     */
-    public boolean isToLowerCase() {
-        return toLowerCase;
     }
 }
