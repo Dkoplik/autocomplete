b36989704e6cb19b7771b36727d690cc02d2df58
Романский Алексей <kirbi.zsthan@gmail.com>
2025-06-20 19:32:03 +0300

    Update Candidate.java

--- src/main/java/io/github/autocomplete/util/Candidate.java
+++ src/main/java/io/github/autocomplete/util/Candidate.java
@@ -3,10 +3,10 @@ package io.github.autocomplete.util;
 /**
  * Кандидат автодополнения
  */
-public record Candidate(String word, int weight) implements Comparable<Candidate> {
+public record Candidate(String word, double weight) implements Comparable<Candidate> {
   @Override
   public int compareTo(Candidate other) {
-    int weightCompare = Integer.compare(this.weight, other.weight);
+    int weightCompare = Double.compare(this.weight, other.weight);
     return weightCompare != 0 ? weightCompare : other.word.compareTo(this.word);
   }
 }
