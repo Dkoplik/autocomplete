d1399cb36060d0e09922ba807e042643f297025c
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-06-21 13:18:56 +0300

    format

--- src/test/java/io/github/autocomplete/AutocompleteConfigTest.java
+++ src/test/java/io/github/autocomplete/AutocompleteConfigTest.java
@@ -1,7 +1,7 @@
 package io.github.autocomplete;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import io.github.autocomplete.util.Levenshtein;
