dd1891c1d074e5deb172a7efd6b653e1b71f556f
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-06-21 14:03:59 +0300

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,35 @@
+/**
+ * Модуль для работы с текстом и автозавершениями.
+ * 
+ * <p>Предоставляет простой текстовый анализатор и автозаполнения, 
+ * причём для автозаполнений присутствует опциональное LRU-кеширование.</p>
+ * 
+ * <p>Основные возможности:</p>
+ * <ul>
+ *   <li>Токенизация и анализ поступаемого текста с подсчётом частот слов</li>
+ *   <li>Поддержка кастомных токенизаторов</li>
+ *   <li>Предложение автозаполнений на основе частот из текстового анализатора</li>
+ *   <li>Поддержка опционального LRU-кеширования для запросов на автозаполнение</li>
+ * </ul>
+ * 
+ * <p>Основные классы:</p>
+ * <ul>
+ *   <li>{@link io.github.autocomplete.AutocompleteProvider} - предоставляет автозаполнения
+ *   на основе частот слов из предоставленного текста</li>
+ *   <li>{@link io.github.autocomplete.TextAnalyzer} - текстовый анализатор текста, проводит
+ *   токенизацию и подсчёт частот слов</li>
+ *   <li>{@link io.github.autocomplete.tokenizer.Tokenizer} - интерфейс токенизатора для TextAnalyzer,
+ *   на случай если работы стандартного токенизатора недостаточно</li>
+ * </ul>
+ * 
+ * @since 1.0
+ */
+module io.github.autocomplete {
+  exports io.github.autocomplete;
+  
+  exports io.github.autocomplete.config;
+  
+  exports io.github.autocomplete.model;
+  
+  exports io.github.autocomplete.tokenizer;
+}
