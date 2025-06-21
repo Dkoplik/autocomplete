8375ad6d9a6cf32531aa8c2478af84c8a40179fc
Коплик Дмитрий <koplikdima2004@gmail.com>
2025-06-21 10:57:01 +0300

    implement Levenshtein distance

---
+++ src/test/java/io/github/autocomplete/util/LevenshteinTest.java
@@ -0,0 +1,37 @@
+package io.github.autocomplete.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevenshteinTest {
+  @Test
+  void distance_IdenticalStrings_ReturnsZero() {
+    assertEquals(0, Levenshtein.distance("kitten", "kitten"));
+    assertEquals(0, Levenshtein.distance("", ""));
+  }
+
+  @Test
+  void distance_EmptyString_ReturnsLengthOfOther() {
+    assertEquals(6, Levenshtein.distance("kitten", ""));
+    assertEquals(6, Levenshtein.distance("", "kitten"));
+  }
+
+  @Test
+  void distance_SingleEditCases() {
+    assertEquals(1, Levenshtein.distance("kitten", "sitten"));
+    assertEquals(1, Levenshtein.distance("kitten", "kitte"));
+    assertEquals(1, Levenshtein.distance("kitten", "kittena"));
+  }
+
+  @Test
+  void distance_MultipleEdits() {
+    assertEquals(3, Levenshtein.distance("kitten", "sitting"));
+    assertEquals(2, Levenshtein.distance("flaw", "lawn"));
+    assertEquals(5, Levenshtein.distance("intention", "execution"));
+  }
+
+  @Test
+  void distance_CaseSensitivity() {
+    assertEquals(1, Levenshtein.distance("abc", "Abc"));
+  }
+} 
